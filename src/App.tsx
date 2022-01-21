b00fcab69f9740337951bb6d5f77521fe86f135e
Sami Ibrahim <sami-dev@hotmail.com>
2022-01-22 00:06:40 +0300

    Prevent default when click a link for a clip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1>App was injected.</h1>;
+  Array.from(document.querySelectorAll(".link-fragment.tw-link")).forEach(
+    (element) => {
+      if (
+        element.getAttribute("href") &&
+        (/^https:\/\/clips\.twitch\.tv\/(?!embed)\S+/.test(
+          element.getAttribute("href") as string
+        ) ||
+          /^https:\/\/www\.twitch\.tv\/(\S+)\/(clip)\/\S+/.test(
+            element.getAttribute("href") as string
+          ))
+      ) {
+        element.addEventListener("click", (el) => {
+          el.preventDefault();
+        });
+      }
+    }
+  );
+  return <h1 style={{ zIndex: 99999999 }}>App was injected.</h1>;
 }
 
 export default App;
